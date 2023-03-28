6c8e7ecbcb45df08b03fcb4dd328d28c72a9abd7
Liguang Zhang <leezhang97@gmail.com>
2023-03-27 21:34:38 -0400

    changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -41,7 +41,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.get('1');
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
